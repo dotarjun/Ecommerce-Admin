b3583a071f4b2c709e7ce91fd7e82bbfc139d5ba
Arjun <arjunsingh8112@gmail.com>
2023-07-11 10:00:37 +0530

    Use State modal hook

---
+++ hooks/UseStateModal.tsx
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface UseStoreModalStore {
+    isOpen: boolean,
+    onOpen: () => void,
+    onClose: () => void,
+}
+
+export const UseStoreModal = create<UseStoreModalStore>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+}))
