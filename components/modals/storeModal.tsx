fd34ddfca896ba18d5c9b17a3bb5da13a75a2e4a
Arjun <arjunsingh8112@gmail.com>
2023-07-11 10:29:26 +0530

    use Zod for form

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -1,10 +1,29 @@
 "use client"
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { UseStoreModal } from "@/hooks/UseStateModal";
 import { Modal } from "@/components/ui/modal";
 
+const formSchema = z.object({
+    name: z.string().min(1),
+})
+
 const StoreModal = () => {
     const storeModal = UseStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        }
+    })
+
+    const onSubmit = async (vaues: z.infer<typeof formSchema>) => {
+        // TODO: Create Score
+    }
 
     return (
         <Modal
