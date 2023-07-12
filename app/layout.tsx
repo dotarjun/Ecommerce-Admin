1dabf1cf1127930e4f5f3589376179cb9037f1de
Arjun <arjunsingh8112@gmail.com>
2023-07-12 10:45:54 +0530

    default formatter to prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import ModalProvider from '@/providers/modalProvider'
+import ModalProvider from "@/providers/modalProvider";
 
-import './globals.css'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard for Guiatr E Commerce Shop',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard for Guiatr E Commerce Shop",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -27,5 +27,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
