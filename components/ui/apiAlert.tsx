43ade335e490ad19cff96d140b1185e7847f3876
Arjun <arjunsingh8112@gmail.com>
2023-07-14 10:30:19 +0530

    style: fix overlapping server icon using grid

--- components/ui/apiAlert.tsx
+++ components/ui/apiAlert.tsx
@@ -34,20 +34,28 @@ export const APIAlert: React.FC<APIAlertProps> = ({
   };
 
   return (
-    <Alert>
-      <Server className="h-4 w-4" />
-      <AlertTitle className="flex items-center gap-x-2">
-        {title}
-        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
-      </AlertTitle>
-      <AlertDescription className="mt-4 flex items-center justify-between">
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          {description}
-        </code>
-        <Button className="ml-2" variant="outline" size="icon" onClick={onCopy}>
-          <Copy className="h-4 w-4" />
-        </Button>
-      </AlertDescription>
+    <Alert className="grid grid-cols-[1rem,auto] gap-2">
+      <div className="w-4 h-4 pt-[2px]">
+        <Server className="h-4 w-4" />
+      </div>
+      <div>
+        <AlertTitle className="flex items-center gap-x-2">
+          {title}
+          <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+        </AlertTitle>
+        <AlertDescription className="mt-4 flex items-center justify-between">
+          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+            {description}
+          </code>
+          <Button
+            className="ml-2"
+            variant="outline"
+            size="icon"
+            onClick={onCopy}>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </AlertDescription>
+      </div>
     </Alert>
   );
 };
