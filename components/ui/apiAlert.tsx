fb089e1be5d074335b31032d439e98be89fa11e6
Arjun <arjunsingh8112@gmail.com>
2023-07-14 09:46:36 +0530

    ui: Add copy icon

--- components/ui/apiAlert.tsx
+++ components/ui/apiAlert.tsx
@@ -1,7 +1,8 @@
-import { Server, ServerIcon } from "lucide-react";
+import { Copy, Server, ServerIcon } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Badge, BadgeProps } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 interface APIAlertProps {
   title: string;
@@ -25,21 +26,20 @@ export const APIAlert: React.FC<APIAlertProps> = ({
   variant = "public",
 }) => {
   return (
-    <Alert className="flex">
-      <div className="h-4 w-4">
-        <Server className="h-4 w-4" />
-      </div>
-      <div>
-        <AlertTitle className="flex items-center gap-x-2">
-          {title}
-          <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
-        </AlertTitle>
-        <AlertDescription>
-          <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-            {description}
-          </code>
-        </AlertDescription>
-      </div>
+    <Alert>
+      <Server className="h-4 w-4" />
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="mt-4 flex items-center justify-between">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button variant="outline" size="icon">
+          <Copy className="h-4 w-4" />
+        </Button>
+      </AlertDescription>
     </Alert>
   );
 };
