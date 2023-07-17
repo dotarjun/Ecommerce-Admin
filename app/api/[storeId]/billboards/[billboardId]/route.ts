769f76b9ddf2d1d6b545602df23c14c7458e6868
Arjun <arjunsingh8112@gmail.com>
2023-07-17 09:57:29 +0530

    new: GET API

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -3,6 +3,28 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 
+export const GET = async (
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) => {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
+
 export const PATCH = async (
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
