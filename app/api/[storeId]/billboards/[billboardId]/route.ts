f8c8ddf08723ee8d261971fa434838fa2d3d9344
Arjun <arjunsingh8112@gmail.com>
2023-07-17 09:54:03 +0530

    new: PATCH API

---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,59 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image URL is required", { status: 400 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorised", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.updateMany({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
