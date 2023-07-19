26d879808385d7ff930ce16c0b3ac6df58f73de5
Arjun <arjunsingh8112@gmail.com>
2023-07-19 12:04:33 +0530

    ui: add dropdown menu UI with Copy,update and delete comps

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
 import { BillboardColumn } from "./columns";
 
 interface CellActionProps {
@@ -7,7 +18,31 @@ interface CellActionProps {
 }
 
 const CellAction: React.FC<CellActionProps> = ({ data }) => {
-  return <div>CellAction</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open Menu</span>
+          <MoreHorizontal className="h-4 w-4 " />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Copy className="mr-2 h-4 w-4" />
+          Copy ID
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Edit className="mr-2 h-4 w-4" />
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default CellAction;
