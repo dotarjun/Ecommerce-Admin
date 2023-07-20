e87f22b86ea8358cafae8296393bcbc152c6a267
Arjun <arjunsingh8112@gmail.com>
2023-07-20 13:03:08 +0530

    feat: pass in categories,sizes, colors and initialData to ProducForm

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -39,7 +39,12 @@ const ProductPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm initialData={product} />
+        <ProductForm
+          categories={categories}
+          sizes={sizes}
+          colors={colors}
+          initialData={product}
+        />
       </div>
     </div>
   );
