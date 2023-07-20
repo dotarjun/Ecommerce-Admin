67c1328287b624eaa352db9207d93ae3c6e6f7ba
Arjun <arjunsingh8112@gmail.com>
2023-07-20 13:02:24 +0530

    bug: use productclient and fix typo

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -43,7 +43,7 @@ const ProductsPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedProducts} />
+        <ProductClient data={formattedProducts} />
       </div>
     </div>
   );
