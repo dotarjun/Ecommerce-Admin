74497b3b2749b974d7a1cccf680cddda01e96def
Arjun <arjunsingh8112@gmail.com>
2023-07-26 20:56:15 +0530

    bug: delete images when Patching

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -105,7 +105,7 @@ export const PATCH = async (
         colorId,
         sizeId,
         images: {
-          deleteMany: [],
+          deleteMany: {},
         },
         isFeatured,
         isArchived,
